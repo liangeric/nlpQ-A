fa96af61729bbeeb25e91d362e2ab970045fa96e
liangeric <liangeric321@gmail.com>
2020-10-27 11:24:10 -0400

    uses nltk to split sentences

--- corpusParsing.py
+++ corpusParsing.py
@@ -1,9 +1,12 @@
-import re
-import sys
-
-# Corpus Parsing to run this follow the command "python corpusParsing.py corpusPath.txt"
+# Corpus Parsing to run this program follow the command "python corpusParsing.py corpusPath.txt"
 # where corpusPath.txt is the path to the corpus text you are reading in
 # an example text to test is Development_data/set1/a1.txt
+
+import re
+import sys
+import nltk
+
+nltk.download('punkt')
 
 if __name__ == '__main__':
 
@@ -21,10 +24,10 @@ if __name__ == '__main__':
     # convert periods with a bunch of spaces/newlines/tabs after to just a period
     # however does not convert if the period and spaces/newlines/tabs is followed by the ) character
     # this helps not filter out cases where the sentence is like (c. 2686-2181 BC)
-    corpus = re.sub(r"\.([\s\n\t]+)(?![^\.\(]*\))",".",corpus)
+    #corpus = re.sub(r"\.([\s\n\t]+)(?![^\.\(]*\))",".",corpus)
 
     # convert the new lines into periods
-    corpus = re.sub(r"[\n]+",".",corpus)
+    corpus = re.sub(r"[\n]+",". ",corpus)
 
     # convert the spaces and tabs into single space
     corpus = re.sub(r"[\s\t]+"," ",corpus)
@@ -32,7 +35,7 @@ if __name__ == '__main__':
     # convert corpus into a vector of sentences and create another copy for word vector later
     # however does not split if the period is followed by a space and then the ) character
     # this helps not split on cases where the sentence is like (c. 2686-2181 BC)
-    corpSen = re.split(r"\.(?! [^\.\(]*\))",corpus)
+    #corpSen = re.split(r"\.(?! [^\.\(]*\))",corpus)
     #corpWords = re.split(r"\.(?! [^\.\(]*\))",corpus)
 
     # convert each vector of sentences into vector of words
@@ -40,7 +43,10 @@ if __name__ == '__main__':
     #for i in range(len(corpWords)):
     #	corpWords[i] = corpWords[i].split(" ")
 
-    print(corpSen[1])
+    # Use nltk to split sentences after cleaning
+    corpSen = nltk.sent_tokenize(corpus)
+
+    print(corpSen)
 
     # We are including references at bottom of each wiki corpus, this is something we may want to remove in the future
     # if it affects our algorithm later
