44413e2d80ed379f714906853fa4c0155eb840fd
Raymond Yang <46874298+raymond-yang@users.noreply.github.com>
2020-11-27 19:18:40 -0500

    ensemble workflow

--- answer.py
+++ answer.py
@@ -43,11 +43,13 @@ class Answer:
 
         self.nlp = spacy.load("en_core_web_lg")  # spacy model
 
-        # read in BERT model and tokenizer
-        self.tokenizer = AutoTokenizer.from_pretrained(
-            "deepset/bert-base-cased-squad2")
-        self.model = AutoModelForQuestionAnswering.from_pretrained(
-            "deepset/bert-base-cased-squad2")
+        # # read in BERT model and tokenizer
+        # self.tokenizer = AutoTokenizer.from_pretrained(
+        #     "deepset/bert-base-cased-squad2")
+        # self.model = AutoModelForQuestionAnswering.from_pretrained(
+        #     "deepset/bert-base-cased-squad2")
+        self.tokenizer = AutoTokenizer.from_pretrained("bert-large-cased-whole-word-masking-finetuned-squad")
+        self.model = AutoModelForQuestionAnswering.from_pretrained("bert-large-cased-whole-word-masking-finetuned-squad")
 
     def preprocess(self):
         """[preprocess the corpus and create spacy objects for corpus and questions]
@@ -229,6 +231,118 @@ class Answer:
             inputs["input_ids"][0][answer_start:answer_end])
         return self.tokenizer.convert_tokens_to_string(correct_tokens)
 
+    def answerBin(self, answerSent, simScore, qobj):
+        """
+        Specific workflow for answering binary questions. Prints {yes, no, no answer}
+        Finds if the question has negation and negation in the answer to attempt to answer yes or nos
+        Args:
+            answerSent: str: the phrase that BERT returns to be the "answer"
+            simScore: float: the cosine similarity of the original answer and the question
+            qObj: question object: the object that holds the question and answerss
+        return:
+            None
+        """
+        questionNeg = 0  # 0 question is not negated, 1 question is negated
+        # Looking for a neg token in the question
+        for t in qobj.spacyDoc:
+            # debugPrint(t.text)
+            if t.dep_ == "neg":
+                questionNeg = 1
+
+        # If the similarity is really low we should just drop
+        if simScore < 0.35:  
+            print("Answer Not Found")
+            return
+
+        # Parsing the answer, for negations of the root verb
+        ansDoc, ans = self.nlp(answerSent), None
+        for token in ansDoc:
+            if token.dep_ == "ROOT":
+                for child in token.children:
+                    if child.head.pos_ == "AUX" and child.dep_ == "neg":
+                        ans = 0
+                        break
+                ans = 1
+                break
+
+        # print answer as is if question is not negated. If question is negated invert answer
+        if questionNeg:
+            if ans:
+                print("No")
+            else:
+                print("Yes")
+        else:
+            if ans:
+                print("Yes")
+            else:
+                print("No")
+
+def ensembleModel(qObjListA, qObjListB):
+    """
+    Ensemble idea to pick top-k from the model that has a higher cosine score. 
+    Args:
+        qObjListA: list of question Object: objects created with model distilroberta-base-msmarco-v2
+        qObjListB: list of question Object: objects created with model roberta-large-nli-stsb-mean-tokens
+    
+    returns:
+        None
+    """
+    qIdx = 0
+    qsObjLst_marco, qsObjLst_roberta = qObjListA, qObjListB
+    for obj_idx in range(len(qsObjLst_marco)):
+        # Get question
+        qObj = qsObjLst_marco[obj_idx]
+        orgQuestion = qObj.raw_question
+        debugPrint("Question {}: {}".format(qIdx, qObj.raw_question))
+        debugPrint(qObj.question_type)
+
+        for i in range(len(qObj.answers)):
+
+            qObj = qsObjLst_marco[obj_idx]
+            marcoScore = qObj.score[i]
+            orgAnswer_marco = qObj.answers[i]
+
+            qObj = qsObjLst_roberta[obj_idx]
+            robertaScore = qObj.score[i]
+            orgAnswer_roberta = qObj.answers[i]
+
+            if robertaScore < marcoScore:
+                debugPrint("Marco was chosen")
+                debugPrint("Answer {}: {} \nCOS SCORE: {}".format(i, orgAnswer_marco,  marcoScore))
+
+                foundAnswer = answer.answerQuestion(orgQuestion, orgAnswer_marco)
+                if foundAnswer != "[CLS]" and foundAnswer.strip() != "":
+                    if qObj.question_type == "BINARY":
+                        answer.answerBin(foundAnswer, qObj.score[i], qObj)
+                        break  # We break since we have answered this question
+                    debugPrint("BERT ANSWER", end=": ")
+                    print(foundAnswer)
+                    break
+                elif i == len(qObj.answers)-1:
+                    debugPrint("BERT ANSWER", end=": ")
+                    print("Answer not found!")
+            else:
+                debugPrint("Roberta was chosen")
+                debugPrint("Answer {}: {} \nCOS SCORE: {}".format(i, orgAnswer_roberta, robertaScore))
+
+                foundAnswer = answer.answerQuestion(orgQuestion, orgAnswer_roberta)
+                if foundAnswer != "[CLS]" and foundAnswer.strip() != "":
+                    if qObj.question_type == "BINARY":
+                        answer.answerBin(foundAnswer, qObj.score[i], qObj)
+                        break  # We break since we have answered this question
+                    debugPrint("BERT ANSWER", end=": ")
+                    print(foundAnswer)
+                    break
+                elif i == len(qObj.answers)-1:
+                    debugPrint("BERT ANSWER", end=": ")
+                    print("Answer not found!")
+            debugPrint("\n")
+
+        debugPrint("\n")
+        qIdx += 1
+    e = time.time()
+    debugPrint(f"Answering took {e-s} Seconds")
+
 
 if __name__ == "__main__":
     s = time.time()
@@ -244,70 +358,14 @@ if __name__ == "__main__":
     # roberta-base-nli-stsb-mean-tokens pretrain semantic textual similarity model
     # distilbert-base-nli-stsb-mean-token also pretrain STS
     # distilroberta-base-msmarco-v2 pretrain for information retrival and 
-    
-    qsObjLst_roberta = answer.similarity(model="roberta-base-nli-stsb-mean-tokens")
+    # roberta-large-nli-stsb-mean-tokens is the larger version of roberta
+
     qsObjLst_marco = answer.similarity(model="distilroberta-base-msmarco-v2")
+    qsObjLst_roberta = answer.similarity(model="roberta-large-nli-stsb-mean-tokens")
+    
 
-    qIdx = 0
-    for obj_idx in range(len(qsObjLst_marco)):
-        # Get question
-        qObj = qsObjLst_marco[obj_idx]
-        orgQuestion = qObj.raw_question
-        debugPrint("Question {}: {}".format(qIdx, qObj.raw_question))
+    ensembleModel(qsObjLst_marco, qsObjLst_roberta)
 
-        for i in range(len(qObj.answers)):
-            # Get answer
-            # debugPrint("MARCO")
-            qObj = qsObjLst_marco[obj_idx]
-            marcoScore = qObj.score[i]
-            orgAnswer_marco = qObj.answers[i]
-            # debugPrint("Answer {}: {} \nCOS SCORE: {}".format(i, orgAnswer_marco, qObj.score[i]))
-            # debugPrint("ROBERTA")
-            qObj = qsObjLst_roberta[obj_idx]
-            robertaScore = qObj.score[i]
-            orgAnswer_roberta = qObj.answers[i]
-            # debugPrint("Answer {}: {} \nCOS SCORE: {}".format(i, orgAnswer, qObj.score[i]))
-            if robertaScore < marcoScore:
-                debugPrint("Marco was chosen")
-                debugPrint("Answer {}: {} \nCOS SCORE: {}".format(i, orgAnswer_marco,  marcoScore))
-                foundAnswer = answer.answerQuestion(orgQuestion, orgAnswer_marco)
-                if foundAnswer != "[CLS]" and foundAnswer.strip() != "":
-                    debugPrint("BERT ANSWER", end=": ")
-                    print(foundAnswer)
-                    break
-                elif i == len(qObj.answers)-1:
-                    debugPrint("BERT ANSWER", end=": ")
-                    print("Answer not found!")
-            else:
-                debugPrint("Roberta was chosen")
-                debugPrint("Answer {}: {} \nCOS SCORE: {}".format(i, orgAnswer_roberta, robertaScore))
-                foundAnswer = answer.answerQuestion(orgQuestion, orgAnswer_roberta)
-                if foundAnswer != "[CLS]" and foundAnswer.strip() != "":
-                    debugPrint("BERT ANSWER", end=": ")
-                    print(foundAnswer)
-                    break
-                elif i == len(qObj.answers)-1:
-                    debugPrint("BERT ANSWER", end=": ")
-                    print("Answer not found!")
-
-            # # debugPrint("Found Answer:")
-            # foundAnswer = answer.answerQuestion(orgQuestion, orgAnswer)
-            # if foundAnswer != "[CLS]" and foundAnswer.strip() != "":
-            #     debugPrint("BERT ANSWER", end=": ")
-            #     print(foundAnswer)
-            #     break
-            # elif i == len(qObj.answers)-1:
-            #     debugPrint("BERT ANSWER", end=": ")
-            #     print("Answer not found!")
-            debugPrint("\n")
-
-        debugPrint("\n")
-        qIdx += 1
-    e = time.time()
-    debugPrint(f"Answering took {e-s} Seconds")
-
-
-    # Used for manual unit testing of a case
     """
     orgAnswer = "Pittsburgh was named in 1758 by General John Forbes, in honor of British statesman William Pitt, 1st Earl of Chatham."
     orgQuestion = "When was Pittsburgh named by General John Forbes, in honor of British statesman William Pitt, 1st Earl of Chatham?"
